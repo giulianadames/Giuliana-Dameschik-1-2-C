18c0f5ed47a786058253fa63a44cb003fac483c7
giulianadames <118322314+giulianadames@users.noreply.github.com>
2022-11-15 22:48:20 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,136 @@
+'use strict';
+
+const productos = [];
+
+productos[0] = [];
+productos[0]['Nombre'] = 'Alfajor de nuez';
+productos[0]['Precio'] = 450;
+productos[0]['Descripcion'] = 'Alfajor relleno de dulce de leche cubierto con coco rayado';
+productos[0]['Imagen'] = 'alfajor.jpg';
+
+productos[1] = [];
+productos[1]['Nombre'] = 'Torta Snickers';
+productos[1]['Precio'] = 650;
+productos[1]['Descripcion'] = 'Torta rellena con el delicioso sabor del chocolate Snickers';
+productos[1]['Imagen'] = 'torta-snicker.jpg';
+
+productos[2] = [];
+productos[2]['Nombre'] = 'Cupcake carrot cake';
+productos[2]['Precio'] = 400;
+productos[2]['Descripcion'] = 'Cupcake de carrot cake con frosting de naranja';
+productos[2]['Imagen'] = 'cupcake.jpg';
+
+productos[3] = [];
+productos[3]['Nombre'] = 'Torta caramelo';
+productos[3]['Precio'] = 650;
+productos[3]['Descripcion'] = 'Bizcochuelo con salsa de caramelo y nueces tostadas';
+productos[3]['Imagen'] = 'torta-caramelo.jpg';
+
+productos[4] = [];
+productos[4]['Nombre'] = 'Cheescake de frutos rojos';
+productos[4]['Precio'] = 650;
+productos[4]['Descripcion'] = 'Cheescake de frutos rojos traído de la Patagonia';
+productos[4]['Imagen'] = 'cheescake-frutos.jpg';
+
+
+productos[5] = [];
+productos[5]['Nombre'] = 'Donas bañadas en chocolate';
+productos[5]['Precio'] = 200;
+productos[5]['Descripcion'] = 'Donas bañadas en chocolate con chips de colores';
+productos[5]['Imagen'] = 'donas.jpg';
+
+productos[6] = [];
+productos[6]['Nombre'] = 'Macarons de arándanos';
+productos[6]['Precio'] = 250;
+productos[6]['Descripcion'] = 'Macarons de arándanos rellenos con mousse de chocolate';
+productos[6]['Imagen'] = 'macarons.jpg';
+
+productos[7] = [];
+productos[7]['Nombre'] = 'Muffins de arándanos';
+productos[7]['Precio'] = 250;
+productos[7]['Descripcion'] = 'Muffins con pedacitos de arándanos';
+productos[7]['Imagen'] = 'muffins.jpg';
+
+productos[8] = [];
+productos[8]['Nombre'] = 'Crumble de frutos rojos';
+productos[8]['Precio'] = 450;
+productos[8]['Descripcion'] = 'Crumble con frutos rojos traídos de la Patagonia';
+productos[8]['Imagen'] = 'crumble.jpg';
+
+
+
+const accesorios = [];
+
+accesorios[0] = [];
+accesorios[0]['Nombre'] = 'Taza COFFE STORE';
+accesorios[0]['Precio'] = 1500;
+accesorios[0]['Descripcion'] = 'Tu taza para disfrutar cada mañana';
+accesorios[0]['Imagen'] = 'taza.jpg';
+
+accesorios[1] = [];
+accesorios[1]['Nombre'] = 'Gorra COFFE STORE';
+accesorios[1]['Precio'] = 1750;
+accesorios[1]['Descripcion'] = 'Nada más lindo que llevar COFFE STORE a todos lados';
+accesorios[1]['Imagen'] = 'gorra.jpg';
+
+accesorios[2] = [];
+accesorios[2]['Nombre'] = 'Cafetera COFFE STORE';
+accesorios[2]['Precio'] = 3500;
+accesorios[2]['Descripcion'] = 'Tomar nuestro café nunca fue tan fácil';
+accesorios[2]['Imagen'] = 'cafetera.jpg';
+
+accesorios[3] = [];
+accesorios[3]['Nombre'] = 'Tote bag COFFE STORE';
+accesorios[3]['Precio'] = 1000;
+accesorios[3]['Descripcion'] = 'Para llevar lo que sea a donde sea';
+accesorios[3]['Imagen'] = 'tote.jpg';
+
+accesorios[4] = [];
+accesorios[4]['Nombre'] = 'Vaso reciclable COFFE STORE';
+accesorios[4]['Precio'] = 1000;
+accesorios[4]['Descripcion'] = 'Para que uses una y otra vez.';
+accesorios[4]['Imagen'] = 'vaso.jpg';
+
+accesorios[5] = [];
+accesorios[5]['Nombre'] = 'Botella térmica COFFE STORE';
+accesorios[5]['Precio'] = 5000;
+accesorios[5]['Descripcion'] = 'Botella térmica de 500 ml.';
+accesorios[5]['Imagen'] = 'botella.jpg';
+
+const cafe = [];
+
+cafe[0] = [];
+cafe[0]['Nombre'] = 'Café Venezolano';
+cafe[0]['Precio'] = 1500;
+cafe[0]['Descripcion'] = 'Café molido Venezuela 250g';
+cafe[0]['Imagen'] = 'venezolano.jpg';
+
+cafe[1] = [];
+cafe[1]['Nombre'] = 'Café Brasilero';
+cafe[1]['Precio'] = 1500;
+cafe[1]['Descripcion'] = 'Café molido Brasil 250g';
+cafe[1]['Imagen'] = 'brasilero.jpg';
+
+cafe[2] = [];
+cafe[2]['Nombre'] = 'Café Africano';
+cafe[2]['Precio'] = 1500;
+cafe[2]['Descripcion'] = 'Café molido África 250g';
+cafe[2]['Imagen'] = 'Africano.jpg';
+
+cafe[3] = [];
+cafe[3]['Nombre'] = 'Café Italiano';
+cafe[3]['Precio'] = 1500;
+cafe[3]['Descripcion'] = 'Café molido Italia 250g';
+cafe[3]['Imagen'] = 'Italiano.jpg';
+
+cafe[4] = [];
+cafe[4]['Nombre'] = 'Café Peruano';
+cafe[4]['Precio'] = 1500;
+cafe[4]['Descripcion'] = 'Café molido Perú 250g';
+cafe[4]['Imagen'] = 'Peruano.jpg';
+
+cafe[5] = [];
+cafe[5]['Nombre'] = 'Café Colombiano';
+cafe[5]['Precio'] = 1500;
+cafe[5]['Descripcion'] = 'Café molido Colombia 250g';
+cafe[5]['Imagen'] = 'Colombiano.jpg';
